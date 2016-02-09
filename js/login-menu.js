52be780a330f424578266feb6a0fdeb17b0557a4
stupiddingo <brent@stupiddingo.com>
2016-02-09 12:08:05 -0700

    Context nav toggle, and update login-menu.js.

--- js/login-menu.js
+++ js/login-menu.js
@@ -4,8 +4,10 @@
  */
 $(document).ready(function () {
   getMainMenu();
-  updateLoginLinks();
   getUser();
+  $("#context-menu-dropdown").click( function() {
+    $("#context-menu-dropdown .glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+  });
 });
 
 // Loads the main menu from IDFG API endpoint
@@ -28,37 +30,43 @@ function getMainMenu() {
   });
 }
 
-// add a return URL to the login URL if not present for post-login redirect
-function updateLoginLinks() {
-  var loginElements = $('.accounts-login-link a');
-  for (var i = 0; i < loginElements.length; i++) {
-    if ($(loginElements[i]).attr('href').indexOf('?returnurl=') < 0) {
-      $(loginElements[i]).attr('href', $(loginElements[i]).attr('href') + '?returnurl=' + window.location.href);
-    }
-  }
-}
-
-// Loads the current user from IDFG API endpoint
-function getUser() {
-  // API request for the current user
-  $.ajax({
-    cache: false
-    , crossDomain: true
-    , dataType: 'jsonp'
-    , success: function (data, requestStatus) {
-      if (requestStatus === 'success') {
-        if (data.user) {
-          updateLoginText(data.user);
-        } else {
-          updateLoginText("Login");
+  // Loads the current user from IDFG API endpoint.
+  function getUser() {
+    // API request for the current user.
+    $.ajax({
+      cache: false
+      , crossDomain: true
+      , dataType: 'jsonp'
+      , success: function (data, requestStatus) {
+        if (requestStatus === 'success') {
+          if (data.user) {
+            updateLoginText(data.user);
+          } else {
+            $('.accounts-login-link a').each(function()
+              {
+                this.href = this.href.replace(/\/accounts/, '/accounts/user/login');
+              });
+            updateLoginText("Login");
+          }
         }
       }
+      , type: 'GET'
+      , url: Drupal.settings.basePath + 'user/state'
+    });
+  }
+  // Convenience function which animates the login text field to a new value,
+  // if not already set to that value.
+  function updateLoginText(newText) {
+    var loginElements = $('.accounts-login-link');
+    for (var i = 0; i < loginElements.length; i++) {
+      if (loginElements[i].textContent.indexOf(newText) < 0) {
+        $(loginElements[i]).fadeOut(400, function () {
+          $(this).find('a .link-text').text(newText);
+          $(this).fadeIn(400);
+        });
+      }
     }
-    , type: 'GET'
-    , url: 'https://idfg.idaho.gov/accounts/user/state'
-  });
-}
-
+  }
 // Convenience function which animates the login text field to a new value, if not already set to that value
 function updateLoginText(newText) {
   var loginElements = $('.accounts-login-link');
