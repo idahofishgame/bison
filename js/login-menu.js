243d008e83263d08b084ba795c046491d3c95dec
stupiddingo <brent@stupiddingo.com>
2016-02-09 13:51:40 -0700

    Fix login-menu.js.

--- js/login-menu.js
+++ js/login-menu.js
@@ -29,48 +29,37 @@ function getMainMenu() {
   });
 }
 
-  // Loads the current user from IDFG API endpoint.
-  function getUser() {
-    var path = 'https://idfg.idaho.gov/accounts';
-    if (location.host == 'fishandgame.idaho.gov') {
-      path = 'https://fishandgame.idaho.gov/ifwis/accounts-net';
-    }
-    // API request for the current user.
-    $.ajax({
-      cache: false
-      , crossDomain: true
-      , dataType: 'jsonp'
-      , success: function (data, requestStatus) {
-        if (requestStatus === 'success') {
-          if (data.user) {
-            updateLoginText(data.user);
-          } else {
-            $('.accounts-login-link a').each(function()
-              {
-                this.href = this.href.replace(/\/accounts/, '/accounts/user/login');
-              });
-            updateLoginText("Login");
-          }
+// Loads the current user from IDFG API endpoint.
+function getUser() {
+  var path = 'https://idfg.idaho.gov/accounts';
+  if (location.host == 'fishandgame.idaho.gov') {
+    path = 'https://fishandgame.idaho.gov/ifwis/accounts-net';
+  }
+  // API request for the current user.
+  $.ajax({
+    cache: false
+    , crossDomain: true
+    , dataType: 'jsonp'
+    , success: function (data, requestStatus) {
+      if (requestStatus === 'success') {
+        if (data.user) {
+          updateLoginText(data.user);
+        } else {
+          $('.accounts-login-link a').each(function()
+            {
+              this.href = this.href.replace(/\/accounts/, '/accounts/user/login');
+            });
+          updateLoginText("Login");
         }
       }
-      , type: 'GET'
-      , url: path + '/user/state'
-    });
-  }
-  // Convenience function which animates the login text field to a new value,
-  // if not already set to that value.
-  function updateLoginText(newText) {
-    var loginElements = $('.accounts-login-link');
-    for (var i = 0; i < loginElements.length; i++) {
-      if (loginElements[i].textContent.indexOf(newText) < 0) {
-        $(loginElements[i]).fadeOut(400, function () {
-          $(this).find('a .link-text').text(newText);
-          $(this).fadeIn(400);
-        });
-      }
     }
-  }
-// Convenience function which animates the login text field to a new value, if not already set to that value
+    , type: 'GET'
+    , url: path + '/user/state'
+  });
+}
+
+// Convenience function which animates the login text field to a new value,
+// if not already set to that value.
 function updateLoginText(newText) {
   var loginElements = $('.accounts-login-link');
   for (var i = 0; i < loginElements.length; i++) {
