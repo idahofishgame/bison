bc318ea138cf4b325f26b4ac674af1d9cd1ac307
stupiddingo <brent@stupiddingo.com>
2016-07-01 13:06:15 -0600

    Centralize updates into 2.1 version.

---
+++ 2.1/js/login-menu.js
@@ -0,0 +1,72 @@
+/**
+ * Loads dynamic login and menu information.  For a live example of
+ * this in use visit http://fishandgame.idaho.gov/ifwis/style.
+ */
+$(document).ready(function () {
+  getMainMenu();
+  getUser();
+  $("#context-menu-dropdown").click( function() {
+    $("#context-menu-dropdown .glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+  });
+});
+
+// Loads the main menu from IDFG API endpoint
+function getMainMenu() {
+  // API request for the menu
+  $.ajax({
+    cache: false
+    , crossDomain: true
+    , dataType: 'jsonp'
+    , success: function (data, requestStatus) {
+      if (requestStatus === 'success') {
+        $('#block-idfg-components-menu').replaceWith(data);
+        getUser();
+      }
+    }
+    , type: 'GET'
+    , url: 'https://idfg.idaho.gov/api/1.0/site/menu'
+  });
+}
+
+// Loads the current user from IDFG API endpoint.
+function getUser() {
+  var path = 'https://idfg.idaho.gov/accounts';
+  if (location.host == 'fishandgame.idaho.gov') {
+    path = 'https://fishandgame.idaho.gov/ifwis/accounts-net';
+  }
+  // API request for the current user.
+  $.ajax({
+    cache: false
+    , crossDomain: true
+    , dataType: 'jsonp'
+    , success: function (data, requestStatus) {
+      if (requestStatus === 'success') {
+        if (data.user) {
+          updateLoginText(data.user);
+        } else {
+          $('.accounts-login-link a').each(function()
+            {
+              this.href = this.href.replace(/\/accounts/, '/accounts/user/login');
+            });
+          updateLoginText("Login");
+        }
+      }
+    }
+    , type: 'GET'
+    , url: path + '/user/state'
+  });
+}
+
+// Convenience function which animates the login text field to a new value,
+// if not already set to that value.
+function updateLoginText(newText) {
+  var loginElements = $('.accounts-login-link');
+  for (var i = 0; i < loginElements.length; i++) {
+    if (loginElements[i].textContent.indexOf(newText) < 0) {
+      $(loginElements[i]).fadeOut(400, function () {
+        $(this).find('a .link-text').text(newText);
+        $(this).fadeIn(400);
+      });
+    }
+  }
+}
