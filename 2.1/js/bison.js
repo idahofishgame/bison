c092a16845ceabfccce0aa4302ef19160517580d
stupiddingo <brent@stupiddingo.com>
2016-07-01 13:26:37 -0600

    Let us switch these shall we.

--- 2.1/js/bison.js
+++ 2.1/js/bison.js
@@ -5,47 +5,9 @@
 (function ($) {
   Drupal.behaviors.bison = {
     attach: function (context, settings) {
-      getUser();
       $("#context-menu-dropdown").click( function() {
         $("#context-menu-dropdown .glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
       });
-    }
-  }
-  // Loads the current user from IDFG API endpoint.
-  function getUser() {
-    // API request for the current user.
-    $.ajax({
-      cache: false
-      , crossDomain: true
-      , dataType: 'jsonp'
-      , success: function (data, requestStatus) {
-        if (requestStatus === 'success') {
-          if (data.user) {
-            updateLoginText(data.user);
-          } else {
-            $('.accounts-login-link a').each(function()
-              {
-                this.href = this.href.replace(/\/accounts/, '/accounts/user/login');
-              });
-            updateLoginText("Login");
-          }
-        }
-      }
-      , type: 'GET'
-      , url: Drupal.settings.basePath + 'user/state'
-    });
-  }
-  // Convenience function which animates the login text field to a new value,
-  // if not already set to that value.
-  function updateLoginText(newText) {
-    var loginElements = $('.accounts-login-link');
-    for (var i = 0; i < loginElements.length; i++) {
-      if (loginElements[i].textContent.indexOf(newText) < 0) {
-        $(loginElements[i]).fadeOut(400, function () {
-          $(this).find('a .link-text').text(newText);
-          $(this).fadeIn(400);
-        });
-      }
     }
   }
 })(jQuery);
