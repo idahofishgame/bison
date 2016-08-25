bfb4ee45f198870cfe94e4245571198bb95f4606
Ben Studer <idahoboy@gmail.com>
2016-08-24 18:23:25 -0600

    Create global.js
    Due to circumstances described at https://idfg.idaho.gov/press/online-sales-suspended, this javascript is to capture active outdoor links from our sites to the press release above.

---
+++ 2.1/js/global.js
@@ -0,0 +1,15 @@
+(function ($) {
+/*
+	This javascript patch is to redirect active traffic to a notification
+  of online sales being halted due to concerns over security.
+*/
+  var search_url = "id.outdoorcentral.us";
+  var replace_url = "https://idfg.idaho.gov/press/online-sales-suspended";
+  
+  $("a").each(function() {
+      var href = $(this).attr('href');
+      if (href.indexOf(search_url)!==-1){
+          $(this).attr("href", replace_url);
+      }
+  });
+})(jQuery);
