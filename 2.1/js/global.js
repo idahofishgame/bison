4f325f296b716275bb338a662c24f5175c3ef4f3
Ben Studer <idahoboy@gmail.com>
2016-08-24 18:33:50 -0600

    Update global.js
    Document needs to be ready. :)

--- 2.1/js/global.js
+++ 2.1/js/global.js
@@ -1,15 +1,18 @@
-(function ($) {
-/*
-	This javascript patch is to redirect active traffic to a notification
-  of online sales being halted due to concerns over security.
-*/
-  var search_url = "id.outdoorcentral.us";
-  var replace_url = "https://idfg.idaho.gov/press/online-sales-suspended";
-  
-  $("a").each(function() {
+(function($) {
+  /*
+  	This javascript patch is to redirect active traffic to a notification
+    of online sales being halted due to concerns over security.
+  */
+  $(document).ready(function() {
+
+    var search_url = "id.outdoorcentral.us";
+    var replace_url = "https://idfg.idaho.gov/press/online-sales-suspended";
+
+    $("a").each(function() {
       var href = $(this).attr('href');
-      if (href.indexOf(search_url)!==-1){
-          $(this).attr("href", replace_url);
+      if (href.indexOf(search_url) !== -1) {
+        $(this).attr("href", replace_url);
       }
+    });
   });
 })(jQuery);
