70f53d81f0ac9032278bf3895e233c20d26a3cc8
stupiddingo <brent@stupiddingo.com>
2017-02-08 17:01:10 -0700

    Fast forward theme to match prod.

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -89,19 +89,19 @@
     <?php endif; ?>
   </div>
   <?php endif; ?>
-</div>      
+</div>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="container">
     <?php print render($page['branding']); ?>
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="//fishandgame.idaho.gov/" title="<?php print t('Home'); ?>">
+      <a class="logo navbar-btn pull-left" href="//idfg.idaho.gov/" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="//fishandgame.idaho.gov/" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <a class="name navbar-brand" href="//idfg.idaho.gov/" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
       <?php endif; ?>
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
@@ -134,7 +134,7 @@
       </div>
     <?php endif; ?><!-- /.navbar-collapse -->
     </div>
-  </div>    
+  </div>
 <div class="main-container container">
 
   <header role="banner" id="page-header-context">
