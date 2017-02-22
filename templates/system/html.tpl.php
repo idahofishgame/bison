564b931912c8dd300831c48544bf42feff90035a
Brent <brent@stupiddingo.com>
2017-02-21 23:07:17 -0700

    Clean up html structure.

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -71,12 +71,12 @@
   <?php print $scripts; ?>
 </head>
 <body<?php print $body_attributes; ?>>
-<div id="skip-link">
-  <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-</div>
-<?php print $page_top; ?>
-<?php print $page; ?>
-<?php print $page_bottom; ?>
-<script src="https://idfg.idaho.gov/global.js"></script>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  <script src="https://idfg.idaho.gov/global.js"></script>
 </body>
 </html>
