49de1687657d6100e3bba1113e015f1b3e46c6a7
Brent <brent@stupiddingo.com>
2017-02-25 01:11:59 -0700

    Removing meta charset to Bootstrap 3.11 via #2849285.

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -58,7 +58,6 @@
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
-  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="apple-touch-icon" href="https://idfg.idaho.gov/apple-touch-icon.png">
   <?php print $head; ?>
